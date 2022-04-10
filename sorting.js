eae03f261867960aa868d15a530e1c97ac8a30e8
navneet dodani <navneet.dodani@walmart.com>
2022-04-10 16:45:13 +0530

    basic UI with generate array function done

---
+++ sorting.js
@@ -0,0 +1,35 @@
+const newBtn = document.getElementById('gen-new-array');
+const genarray = () =>{
+    let min = 10;
+    let max = 200;
+    let arr = [];
+    const size = document.getElementById('size_input').value;
+    console.log(size);
+    for(let i=0;i<size;i++){
+        arr.push(Math.floor(Math.random() * (max - min) + min));
+    }
+    createdivs(arr);
+}
+
+
+const createdivs = (arr) =>{
+    // clear existing divs if any
+    deletebars();
+    const bars = document.querySelector('#sorting-wrapper');
+    for(let i=0;i<arr.length;i++){
+        const bar = document.createElement("div");
+        bar.style.height = `${arr[i]*2}px`;
+        bar.classList.add('bar');
+        bar.classList.add('flex-item');
+        bar.classList.add(`barNo${i}`);
+        bar.innerHTML = i;
+        console.log(bar);
+        bars.appendChild(bar);
+    }
+}
+
+const deletebars = () => {
+    const bars = document.querySelector('#sorting-wrapper');
+    bars.innerHTML='';
+}
+newBtn.addEventListener('click',genarray);
